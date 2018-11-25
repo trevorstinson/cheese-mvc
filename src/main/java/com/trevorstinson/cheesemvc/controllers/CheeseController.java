ff9695aca717ce0b4bd6ee3e47cf6c9eb900ac7e
Trevor Stinson <trevor@trevorstinson.com>
2018-11-25 15:11:25 -0600

    Initial commit

---
+++ src/main/java/com/trevorstinson/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,17 @@
+package com.trevorstinson.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+}
